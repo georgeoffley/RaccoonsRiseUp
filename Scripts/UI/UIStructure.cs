b222019853bfc68e4cee0cd9b8f82bc25b04d240
George Offley <geo.offley@gmail.com>
2023-08-16 09:28:51 -0400

    Updated structure class to correct data type for cost

--- Scripts/UI/UIStructure.cs
+++ Scripts/UI/UIStructure.cs
@@ -2,68 +2,68 @@ namespace RRU;
 
 public partial class UIStructure : Node
 {
-	// 'int cost' needs to be replaced with an array of resources required
-	// to purchase this structure. For example a structure could cost
-	// 50 tech points, 150 wood and 200 stone
-	Dictionary<ResourceType, int> cost;
-	string name;
-	string prevLineEditCountText;
-	LineEdit lineEditCount;
+    // 'int cost' needs to be replaced with an array of resources required
+    // to purchase this structure. For example a structure could cost
+    // 50 tech points, 150 wood and 200 stone
+    Dictionary<ResourceType, int> cost;
+    string name;
+    string prevLineEditCountText;
+    LineEdit lineEditCount;
 
-	public override void _Ready()
-	{
-		lineEditCount = GetNode<LineEdit>("%Count");
-		lineEditCount.TextChanged += EnforceOnlyNumbersOnCountLineEdit;
-		GetNode<Button>("%Purchase").Pressed += HandlePurchase;
-	}
+    public override void _Ready()
+    {
+        lineEditCount = GetNode<LineEdit>("%Count");
+        lineEditCount.TextChanged += EnforceOnlyNumbersOnCountLineEdit;
+        GetNode<Button>("%Purchase").Pressed += HandlePurchase;
+    }
 
-	public UIStructure UpdateName(string name)
-	{
-		this.name = name;
-		GetNode<Label>("%Name").Text = name;
-		return this;
-	}
+    public UIStructure UpdateName(string name)
+    {
+        this.name = name;
+        GetNode<Label>("%Name").Text = name;
+        return this;
+    }
 
-	public UIStructure UpdateCost(Dictionary<ResourceType, int> cost)
-	{
-		this.cost = cost;
-		GetNode<Label>("%Cost").Text = cost.ToString();
-		return this;
-	}
+    public UIStructure UpdateCost(Dictionary<ResourceType, int> cost)
+    {
+        this.cost = cost;
+        GetNode<Label>("%Cost").Text = cost.ToString();
+        return this;
+    }
 
-	// The param should not be of type 'TechType' but rather 'StructureType'
-	// but then this means that techs and structures both share the same image
-	// data in common. The code should be refactored around this somehow.
-	public UIStructure UpdateIcon(TechType techType)
-	{
-		GetNode<TextureRect>("%Icon").Texture = 
-			Game.TechData[techType].GetImage();
+    // The param should not be of type 'TechType' but rather 'StructureType'
+    // but then this means that techs and structures both share the same image
+    // data in common. The code should be refactored around this somehow.
+    public UIStructure UpdateIcon(TechType techType)
+    {
+        GetNode<TextureRect>("%Icon").Texture = 
+            Game.TechData[techType].GetImage();
 
-		return this;
-	}
+        return this;
+    }
 
-	void HandlePurchase()
-	{
-		if (!int.TryParse(lineEditCount.Text, out int amount))
-			return;
-		int totalCostOfStructure = totalCost();
+    void HandlePurchase()
+    {
+        if (!int.TryParse(lineEditCount.Text, out int amount))
+            return;
+        int totalCostOfStructure = totalCost();
 
-		GD.Print($"Purchased {amount} {name} for {totalCostOfStructure * amount} wood");
-	}
+        GD.Print($"Purchased {amount} {name} for {totalCostOfStructure * amount} wood");
+    }
 
-	void EnforceOnlyNumbersOnCountLineEdit(string text)
-	{
-		if (!text.IsDigitsOnly())
-		{
-			lineEditCount.Text = prevLineEditCountText;
-			return;
-		}
+    void EnforceOnlyNumbersOnCountLineEdit(string text)
+    {
+        if (!text.IsDigitsOnly())
+        {
+            lineEditCount.Text = prevLineEditCountText;
+            return;
+        }
 
-		prevLineEditCountText = text;
-	}
+        prevLineEditCountText = text;
+    }
 
-	private int totalCost()
-	{
-		return this.cost.Sum(cost => cost.Value);
-	}
+    private int totalCost()
+    {
+        return this.cost.Sum(cost => cost.Value);
+    }
 }
